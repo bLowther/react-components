0f35a92e7d647e7875ae1f9487414d0dac7eae42
Brian Lowther <merchanthagen@gmail.com>
2021-10-04 14:47:21 -0700

    Implemented a grocery list with bold on hover

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bold: false
+    };
+  }
+  onListItemHover() {
+    this.setState({
+      bold: !this.state.bold
+    });
+  }
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)}
+      onMouseLeave={this.onListItemHover.bind(this)}>
+        {this.props.item}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => <GroceryListItem item = {item}/>)}
+  </ul>
+);
+var list = ['coffee', 'beer']
+ReactDOM.render(<GroceryList items={list}/>, document.getElementById("app"));
+
+
